c658965f82aeee59975a4c6d573ecf3207ffe705
Philip Andes <philip__andes@hotmail.nl>
2019-06-05 11:24:19 +0200

    Creating a component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  // Its also possible to do it like this:
+  // template: '<h2>Philip Andes</h2>'
+  // styles: [`
+  //   h2 {
+  //     color: blue
+  //   }
+  // `]
+})
+
+export class UserComponent {
+
+}
